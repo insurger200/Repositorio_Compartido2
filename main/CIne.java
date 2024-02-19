c2882992fc581a81ffa5c4b5724d688838f3122e
unknown <miniinsurger@gmail.com>
2024-02-19 22:50:00 +0100

    Prim_Part_Cine_Doc

---
+++ main/CIne.java
@@ -0,0 +1,12 @@
+package Clases;
+
+import java.util.ArrayList;
+
+import Clases.Sala;
+import Clases.Pelicula;
+
+public class Cine {
+    private String nombre;
+    private int aforo;
+    private Sala[] salas;
+    Pelicula arraypelicula[] = {new Pelicula("KingKong", 33), new Pelicula("MarioBros", 21), new Pelicula("Barbie", 45)};
