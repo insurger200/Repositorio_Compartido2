452ecd123a3e8301780cf34ab6f4dd1a7e02e38b
unknown <miniinsurger@gmail.com>
2024-02-19 22:51:00 +0100

    Cine_actualizado_feat

--- main/CIne.java
+++ main/CIne.java
@@ -10,3 +10,12 @@ public class Cine {
     private int aforo;
     private Sala[] salas;
     Pelicula arraypelicula[] = {new Pelicula("KingKong", 33), new Pelicula("MarioBros", 21), new Pelicula("Barbie", 45)};
+
+    public Cine(String nombre,int filas,int columnas, int numerosalas, int aforo) {
+        this.nombre = nombre;
+        this.aforo = aforo;
+        salas = new Sala[numerosalas];
+        for( int i = 0; i<salas.length;i++) {
+            salas[i] = new Sala(i, null, columnas, filas);
+        }
+    }
