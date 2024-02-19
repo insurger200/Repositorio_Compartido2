1383be71d2cb954cbef1ca1be3bb16ddd12f981f
unknown <miniinsurger@gmail.com>
2024-02-19 22:54:36 +0100

    Cine_Comentado_Actualizado_perf

--- main/CIne.java
+++ main/CIne.java
@@ -9,6 +9,7 @@ public class Cine {
     private String nombre;
     private int aforo;
     private Sala[] salas;
+    //Se le ponen los arrays
     Pelicula arraypelicula[] = {new Pelicula("KingKong", 33), new Pelicula("MarioBros", 21), new Pelicula("Barbie", 45)};
 
     public Cine(String nombre,int filas,int columnas, int numerosalas, int aforo) {
@@ -31,9 +32,10 @@ public class Cine {
         for (int i = 0; i<salas.length;i++){
             getSalas()[i].setPelicula(arraypelicula[i]);
             System.out.println("SALA " + (i+1));
+            // Este método devuelve el título de la película almacenado en el atributo titulo
             System.out.println("PELICULA; " + getSalas()[i].getPelicula().getTitulo() + "\nDURACION: " + getSalas()[i].getPelicula().getDuracion());
         }
-
+// Se hacen diferentes public
     }
     public String getNombre()  {
         return nombre;
