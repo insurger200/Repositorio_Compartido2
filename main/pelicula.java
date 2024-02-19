38e67ec2f3a2b1b15116518e9ecf0d081ba9e27d
unknown <miniinsurger@gmail.com>
2024-02-19 23:25:27 +0100

    Alex_pelicula_Doc

---
+++ main/pelicula.java
@@ -0,0 +1,11 @@
+package Clases;
+
+public class Pelicula {
+
+private String titulo;
+private int duracion;
+// Es la primera pelicula
+public Pelicula(String titulo, int duracion){
+    this.titulo = titulo;
+    this.duracion = duracion;
+}
